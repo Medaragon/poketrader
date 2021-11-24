7b467dd226177b092ffef135a1a36fa021b5985d
Marc-Eric Pelletier <medaragon@hotmail.com>
2021-11-23 22:47:21 -0500

    Create a base pokemon list with PokemonCards

---
+++ poketrader-frontend/src/components/PokemonCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  pokemon: Object
+})
+</script>
+
+<template>
+  <div class="card">
+    <img class="sprite" :src="props.pokemon.sprite" />
+    <div>
+      <span>{{ props.pokemon.id }} - </span>
+      <span class="capitalize">{{ props.pokemon.name }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 5px;
+  height: 150px;
+  width: 150px;
+
+  .sprite {
+    object-fit: contain;
+    height: 90%;
+    width: 90%;
+  }
+
+  .capitalize {
+    text-transform: capitalize;
+  }
+}
+</style>
--- poketrader-frontend/src/components/PokemonList.vue
+++ poketrader-frontend/src/components/PokemonList.vue
@@ -1,1 +1,29 @@
-<script></script>
+<script setup>
+import { ref } from 'vue'
+import PokeApi from '@/services/pokeapi.js'
+import PokemonCard from './PokemonCard.vue'
+
+const pokemons = ref([])
+PokeApi.fetchPokemons().then((result) => (pokemons.value = result))
+</script>
+
+<template>
+  <div class="list">
+    <PokemonCard
+      v-for="(pokemon, index) in pokemons"
+      :key="'pokemon' + index"
+      :pokemon="pokemon"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.list {
+  display: flex;
+  flex-wrap: wrap;
+
+  > * {
+    margin: 10px;
+  }
+}
+</style>
