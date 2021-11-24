c2b1d03843e08966ba407673499c7facb6b63eda
Marc-Eric Pelletier <medaragon@hotmail.com>
2021-11-23 22:45:39 -0500

    Remove semicolons from eslint

--- poketrader-frontend/.eslintrc.js
+++ poketrader-frontend/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
     quotes: ['warn', 'single'],
+    semi: ['off'],
     'vue/multi-word-component-names': ['off']
   }
 }
